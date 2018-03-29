91d6e5174868ddc3226da9567a4ce8154a9a65e4
Luke-Clements <36176436+Luke-Clements@users.noreply.github.com>
2018-03-29 11:59:48 -0700

    First look at private varaibles
    Need to write constructor, get, and set methods

---
+++ BackCode/CategoryWeight.java
@@ -0,0 +1,17 @@
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.StringProperty;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Contains the categories of the grading scale and the weight of each category
+ * @author lukecjm
+ */
+public class CategoryWeight {
+    private StringProperty cwCategory;
+    private IntegerProperty cwWeight;
+}
