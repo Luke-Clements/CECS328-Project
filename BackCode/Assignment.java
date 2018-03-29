5ab6439283af091e7908c9c059c2c24992642110
Luke-Clements <36176436+Luke-Clements@users.noreply.github.com>
2018-03-29 11:59:03 -0700

    first look at assignment private values
    need to write constructor, get, and set methods

---
+++ BackCode/Assignment.java
@@ -0,0 +1,18 @@
+import javafx.beans.property.FloatProperty;
+import javafx.beans.property.StringProperty;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Contains the name, category, and score of each assignment in the class
+ * @author lukecjm
+ */
+public class Assignment {
+    private StringProperty aName;
+    private StringProperty aCategory; //category in the grading scale
+    private FloatProperty aScore;
+}
