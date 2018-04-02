82d1c7056d93207cd06ec1cd9bc5b591ac4e1537
Luke-Clements <36176436+Luke-Clements@users.noreply.github.com>
2018-04-01 22:38:17 -0700

    Requires get/set methods
    only variables noted in here. this is the class to store the program user's information

---
+++ BackCode/User.java
@@ -0,0 +1,19 @@
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.StringProperty;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lukecjm
+ */
+public class User {
+    private StringProperty uName;
+    private IntegerProperty uIDNumber;
+    private BooleanProperty uIsTeacher;
+}
