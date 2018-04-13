9025d2553329e84691b886e2c62b082560c69acd
Luke-Clements <36176436+Luke-Clements@users.noreply.github.com>
2018-04-12 18:08:51 -0700

    settings for filepath to save files and user information
    Needs get/set methods

---
+++ BackCode/Settings.java
@@ -0,0 +1,19 @@
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.StringProperty;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lukecjm
+ */
+public class Settings 
+{
+    private StringProperty filePathToDataBaseFile;
+    private StringProperty userName;
+    private IntegerProperty idNumber;
+}
