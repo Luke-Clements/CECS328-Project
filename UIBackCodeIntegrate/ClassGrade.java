c78bd83bca831424e288ad017287c5acf8efec8e
MarcM1250 <36463798+MarcM1250@users.noreply.github.com>
2018-04-24 18:42:44 -0700

    Update ClassGrade.java

--- UIBackCodeIntegrate/ClassGrade.java
+++ UIBackCodeIntegrate/ClassGrade.java
@@ -24,14 +24,45 @@ public class ClassGrade
     
     public ClassGrade()
     {
+        /* Placeholder Values
         className = new SimpleStringProperty("CECS 343");
         professorName = new SimpleStringProperty("Sharifian");
         grade = new SimpleStringProperty("A");
         semester = new SimpleStringProperty("SPRING18");
         schoolName = new SimpleStringProperty("CSULB");
         assignment = new Assignment();
+        */
+        
+        className = null;
+        professorName = null;
+        grade = null;
+        semester = null;
+        schoolName = null;
+        assignment = new Assignment();
     }
     
+    //Setters
+    public void setName(StringProperty cName){
+        className = cName;
+    }
+    
+    public void setProfName(StringProperty pName){
+        professorName = pName;
+    }
+    
+    public void setGrade(StringProperty cGrade){
+        grade = cGrade;
+    }
+    
+    public void setSemester(StringProperty cSemester){
+        semester = cSemester;
+    }
+    
+    public void setSchoolName(StringProperty cSchoolName){
+        schoolName = cSchoolName;
+    }
+    
+    //Getters
     public Assignment getAssignment()
     {
         return assignment;
