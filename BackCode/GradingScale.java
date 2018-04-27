edfce40802f4b37a89c91a76b3fdd44cb1750ca0
vigilmiguel <36463350+vigilmiguel@users.noreply.github.com>
2018-04-27 01:01:26 -0700

    Update GradingScale.java

--- BackCode/GradingScale.java
+++ BackCode/GradingScale.java
@@ -2,6 +2,10 @@ package BackCode;
 
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.FloatProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+
+
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -13,12 +17,71 @@ import javafx.beans.property.FloatProperty;
  * Contains the grading values as well as the grading scale from the CategoryWeight class
  * @author lukecjm
  */
-public class GradingScale {
-    private FloatProperty A;
+public class GradingScale 
+{
+    private FloatProperty A; // represents the lowest score to obtain that grade.
     private FloatProperty B;
     private FloatProperty C;
     private FloatProperty D;
     private FloatProperty F;
     private BooleanProperty gsPassFail; //indicates whether the class is a pass/fail assessment
-    private CategoryWeight[] gsGradingScale;
+    private CategoryWeight gsGradingScale;
+    
+    // Default Grading Scale
+    void GradingScale()
+    {
+        A.set(90);
+        B.set(80);
+        C.set(70);
+        D.set(60);
+        F.set(0);
+        gsPassFail.set(false);
+    }
+    
+    // Allow the user to customize grade percentages.
+    void GradingScale(FloatProperty a, FloatProperty b, FloatProperty c, FloatProperty d, FloatProperty f)
+    {
+        A = a;
+        B = b;
+        C = c;
+        D = d;
+        F = f;
+        gsPassFail.set(false);
+    }
+    
+    // If this is a pass/fail, 
+    void GradingScale(FloatProperty c)
+    {
+        C = c;
+        gsPassFail.set(true);
+    }
+    
+    public void setCategoryWeights(CategoryWeight cw)
+    {
+        gsGradingScale = cw;
+    }
+    
+    public CategoryWeight getCategoryWeights()
+    {
+        return gsGradingScale;
+    }
+    
+    public FloatProperty getLowestScore(SimpleStringProperty grd)
+    {
+        
+        char grade = grd.get().charAt(0);
+        
+        
+        switch( Character.toUpperCase(grade) )
+        {
+            case 'A': return A;
+            case 'B': return B;
+            case 'C': return C;
+            case 'D': return D;
+            default: return F;
+        }
+    }
+    
+    
+   
 }
