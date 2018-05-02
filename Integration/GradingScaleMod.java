7c7d2c52b773ddc6c9986f99627e2a16ab058065
Luke-Clements <36176436+Luke-Clements@users.noreply.github.com>
2018-05-02 12:32:31 -0700

    Update GradingScaleMod.java
    finished

--- Integration/GradingScaleMod.java
+++ Integration/GradingScaleMod.java
@@ -5,6 +5,11 @@
  */
 package Integration;
 
+import BackCode.Settings;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import javafx.collections.FXCollections;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -22,54 +27,128 @@ public class GradingScaleMod
     private static final String[] GRADING_SCALE_INFO_CATEGORIES = {"A", "B", "C", "D", "F", "Pass or Fail"};
     public static final String[] GRADING_SCALE_INFO_EMPTY = {"", "", "", "", "", ""};
     
+    private static TextField aText;
+    private static TextField bText;
+    private static TextField cText;
+    private static TextField dText;
+    private static TextField fText;
+    private static ComboBox passFailBox;
     public static void SetupGradingScaleMod(VBox gradingScaleMod, String[] gsi)
     {
         gradingScaleMod.getChildren().clear();
         HBox a = new HBox();
-        TextField aText = new TextField();
+        aText = new TextField();
         Label promptA = new Label(GRADING_SCALE_INFO_CATEGORIES[0] + ":");
         aText.setText(gsi[0]);
         a.getChildren().addAll(promptA, aText);
         
         HBox b = new HBox();
-        TextField bText = new TextField();
+        bText = new TextField();
         Label promptB = new Label(GRADING_SCALE_INFO_CATEGORIES[1] + ":");
         bText.setText(gsi[1]);
         b.getChildren().addAll(promptB, bText);
         
         HBox c = new HBox();
-        TextField cText = new TextField();
+        cText = new TextField();
         Label promptC = new Label(GRADING_SCALE_INFO_CATEGORIES[2] + ":");
         cText.setText(gsi[2]);
         c.getChildren().addAll(promptC, cText);
         
         HBox d = new HBox();
-        TextField dText = new TextField();
+        dText = new TextField();
         Label promptD = new Label(GRADING_SCALE_INFO_CATEGORIES[3] + ":");
         dText.setText(gsi[3]);
         d.getChildren().addAll(promptD, dText);
         
         HBox f = new HBox();
-        TextField fText = new TextField();
+        fText = new TextField();
         Label promptF = new Label(GRADING_SCALE_INFO_CATEGORIES[4] + ":");
         fText.setText(gsi[4]);
         f.getChildren().addAll(promptF, fText);
         
         HBox passFail = new HBox();
-        ComboBox passFailBox = new ComboBox();
+        passFailBox = new ComboBox();
         String[] trueFalse = {"True", "False"};
         Label promptpassFail = new Label(GRADING_SCALE_INFO_CATEGORIES[5] + ":");
         passFailBox.setItems(FXCollections.observableArrayList(trueFalse));
         passFailBox.setValue(gsi[5]);
         passFail.getChildren().addAll(promptpassFail, passFailBox);
         
-        Button saveGradingScale = new Button("Save Grading Scale Changes");
-
-        saveGradingScale.setOnMouseClicked(e -> 
-        {
-            //need to execute a update statement in the database
-        });
+        gradingScaleMod.getChildren().addAll(a, b, c, d, f, passFail);
+    }
+    
+    public static String getGradingScaleID(Settings settings, Connection conn)
+    {
+        String stmt = "SELECT gsID FROM GradingScale where A= " + Float.parseFloat(aText.getText()) +
+                                                     " and B= " + Float.parseFloat(bText.getText()) +
+                                                     " and C= " + Float.parseFloat(cText.getText()) +
+                                                     " and D= " + Float.parseFloat(dText.getText()) +
+                                                     " and F= " + Float.parseFloat(fText.getText()) +
+                                                     " and passFail = " + passFailBox.getSelectionModel().getSelectedItem();
         
-        gradingScaleMod.getChildren().addAll(a, b, c, d, f, passFail, saveGradingScale);
+        ResultSet rs = null;
+        
+        try
+        {
+            PreparedStatement ps = conn.prepareStatement(stmt);
+            
+            rs = ps.executeQuery();
+            
+            rs.next();
+            return rs.getString("gsID");
+        }
+        catch(SQLException se)
+        {
+            if(se.getMessage().equals("Invalid cursor state - no current row."))
+            {
+                InsertNewGradingScale(settings, conn);
+                return getGradingScaleID(settings, conn);
+            }
+//            se.printStackTrace();
+        }
+        return null;
+    }
+    
+    public static void InsertNewGradingScale(Settings settings, Connection conn)
+    {
+        String Stmt = "INSERT INTO GradingScale(gsID, A, B, C, D, F, passFail) "+
+                "VALUES(?, ?, ?, ?, ?, ?, ?)";
+           
+        float a = Float.parseFloat(aText.getText());
+        float b = Float.parseFloat(bText.getText());
+        float c = Float.parseFloat(cText.getText());
+        float d = Float.parseFloat(dText.getText());
+        float f = Float.parseFloat(fText.getText());
+        boolean passFail = false;
+
+        try
+        {
+            if(aText.getText().equals("") || bText.getText().equals("") || cText.getText().equals("") || dText.getText().equals("") || fText.getText().equals(""))
+            {
+                throw new NullPointerException("Cannot have empty fields.");
+            }
+            PreparedStatement pStmt = conn.prepareStatement(Stmt);
+            pStmt.setInt(1, settings.getGradingScaleCounter());
+            pStmt.setFloat(2,a);
+            pStmt.setFloat(3,b);
+            pStmt.setFloat(4,c);
+            pStmt.setFloat(5,d);
+            pStmt.setFloat(6, f);
+            pStmt.setBoolean(7, passFail);
+
+            pStmt.executeUpdate();
+            System.out.println("executed update");
+            settings.incrementGradingScaleCounter();
+        }
+        catch(SQLException se)
+        {
+            se.printStackTrace();
+        }
+        catch(NullPointerException npe)
+        {
+            System.out.println("null somewhere");
+            npe.printStackTrace();
+            //notification window
+        }
     }
 }
