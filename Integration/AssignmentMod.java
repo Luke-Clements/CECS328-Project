66d1aa174bb586aee34743326044e94c69316353
Luke-Clements <36176436+Luke-Clements@users.noreply.github.com>
2018-05-02 13:59:40 -0700

    Update AssignmentMod.java
    finished as far as I know

--- Integration/AssignmentMod.java
+++ Integration/AssignmentMod.java
@@ -6,7 +6,16 @@
 package Integration;
 
 import BackCode.Assignment;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Time;
 import java.util.ArrayList;
+import javafx.beans.property.SimpleFloatProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleLongProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.Button;
@@ -26,16 +35,22 @@ public class AssignmentMod
 {
     private static final String[] ASSIGNMENT_INFO_CATEGORIES = {"Assignment Name", "Category", "Score", "Max Score"};
     public static final String[] ASSIGNMENT_INFO_EMPTY = {"", "", "", ""};
-    private static ObservableList<Assignment> assignmentItems;
+    private ObservableList<Assignment> assignmentItems;
+    private TableView<Assignment> assignmentTable = new TableView();
     
     //sets up Assignment table
-    public static TableView<Assignment> SetupAssignmentTable(VBox assignmentModBox, int classID)
+    public TableView<Assignment> SetupAssignmentTable(Connection conn, VBox assignmentModBox, ClassMod cm)
     {
-        TableView<Assignment> assignmentTable = new TableView<>();
-        ArrayList<Assignment> ai = new ArrayList();
-        ai.add(new Assignment());
-        
-        assignmentItems = FXCollections.observableList(ai);
+        int classID;
+        if(!(cm.getClassTable() == null))
+        {
+            classID = cm.getClassTable().getSelectionModel().getSelectedItem().getCID();
+        }
+        else
+        {
+            classID = -1;
+        }
+        assignmentItems = GetAssignmentTableValues(classID, conn);
         
         assignmentTable.setItems(assignmentItems);
         
@@ -51,14 +66,14 @@ public class AssignmentMod
         assignmentTable.getSelectionModel().selectedIndexProperty().addListener(e -> {
             Assignment a = assignmentTable.getSelectionModel().getSelectedItem();
             String[] assignmentInfo = a.getAssignmentInfoArray();
-            SetupAssignmentMod(assignmentModBox, assignmentInfo);
+            SetupAssignmentMod(classID, conn, assignmentModBox, assignmentInfo);
         });
 
         assignmentTable.getColumns().setAll(aNameCol, aCategoryCol, aScoreCol, aMaxScoreCol);
         return assignmentTable;
     }
     
-    public static void SetupAssignmentMod(VBox assignmentMod, String[] assignmentInfo)
+    public void SetupAssignmentMod(int classID, Connection conn, VBox assignmentMod, String[] assignmentInfo)
     {
         assignmentMod.getChildren().clear();
         HBox getAssignmentName = new HBox();
@@ -89,9 +104,81 @@ public class AssignmentMod
 
         saveAssignment.setOnMouseClicked(e -> 
         {
-            //need to execute a update statement in the database
+            String Stmt = "INSERT INTO Assignment(cID, aName, aCategory, score, maxScore) "+
+                "VALUES(?, ?, ?, ?, ?)";
+           
+            //perform category check in categoryweight
+            String aName = nameField.getText();
+            String aCategory = categoryField.getText();
+            float score = Float.parseFloat(scoreField.getText());
+            float maxScore = Float.parseFloat(maxScoreField.getText());
+
+            try
+            {
+                if(aName.equals("") || aCategory.equals("") || scoreField.getText().equals("") || maxScoreField.getText().toString().equals(""))
+                {
+                    throw new NullPointerException("Cannot have empty fields.");
+                }
+                PreparedStatement pStmt = conn.prepareStatement(Stmt);
+                pStmt.setInt(1, classID);
+                pStmt.setString(2,aName);
+                pStmt.setString(3,aCategory);
+                pStmt.setFloat(4,score);
+                pStmt.setFloat(5,maxScore);
+
+                pStmt.executeUpdate();
+            }
+            catch(SQLException se)
+            {
+                se.printStackTrace();
+            }
+            catch(NullPointerException npe)
+            {
+                System.out.println("null somewhere");
+                npe.printStackTrace();
+                //notification window
+            }
+            assignmentItems = GetAssignmentTableValues(classID, conn);
+            for(Assignment a:assignmentItems)
+            {
+                System.out.println(a.getAssignmentName());
+            }
+            assignmentTable.setItems(assignmentItems);            
         });
         
         assignmentMod.getChildren().addAll(getAssignmentName, getCategory, getScore, getMaxScore, saveAssignment);
+    }
+    
+    public static ObservableList<Assignment> GetAssignmentTableValues(int classID, Connection conn)
+    {
+        String stmt = "SELECT * FROM Assignment WHERE cID=" + classID;
+        ArrayList<Assignment> aa = new ArrayList();
+        
+        try
+        {
+            PreparedStatement ps = conn.prepareStatement(stmt);
+            ResultSet rs = ps.executeQuery();
+            Assignment a = new Assignment();
+            
+            aa.add(a);
+
+            if(rs.next())
+            {
+                do
+                {
+                    a = new Assignment();
+                    a.setName(new SimpleStringProperty(rs.getString("aName")));
+                    a.setCategory(new SimpleStringProperty(rs.getString("aCategory")));
+                    a.setScore(new SimpleFloatProperty(rs.getFloat("score")));
+                    a.setMaxScore(new SimpleFloatProperty(rs.getFloat("maxScore")));
+                    aa.add(a);
+                }while(rs.next());
+            }
+        }
+        catch(SQLException se)
+        {
+            se.printStackTrace();
+        }
+        return FXCollections.observableArrayList(aa);
     }
 }
