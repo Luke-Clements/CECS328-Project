e0f6422258826173805a48384c22ee8e6a24cbd3
Luke-Clements <36176436+Luke-Clements@users.noreply.github.com>
2018-05-03 15:12:48 -0700

    Update ClassGrade.java

--- UIBackCodeIntegrate/ClassGrade.java
+++ UIBackCodeIntegrate/ClassGrade.java
@@ -23,21 +23,12 @@ public class ClassGrade
     
     public ClassGrade()
     {
-        // Placeholder Values
-        className = new SimpleStringProperty("CECS 343");
-        professorName = new SimpleStringProperty("Sharifian");
-        grade = new SimpleStringProperty("A");
-        semester = new SimpleStringProperty("SPRING18");
-        schoolName = new SimpleStringProperty("CSULB");
-        
-        
-//        className = null;
-//        professorName = null;
-//        grade = null;
-//        semester = null;
-//        schoolName = null;
-//        assignment = new Assignment();
-    }
+        className = null;
+        professorName = null;
+        grade = null;
+        semester = null;
+        schoolName = null;
+   }
     
     //Setters
     public void setName(StringProperty cName){
