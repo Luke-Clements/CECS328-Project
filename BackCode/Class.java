6ae0b4c2e64b1733975e30a96c766a0a146b4b75
Luke-Clements <36176436+Luke-Clements@users.noreply.github.com>
2018-05-04 03:11:06 -0700

    Update Class.java

--- BackCode/Class.java
+++ BackCode/Class.java
@@ -1,9 +1,7 @@
 package BackCode;
 
 import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.LongProperty;
 import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleLongProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
