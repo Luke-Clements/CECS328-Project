e7591ed7bd910f6e37ae36fa72b30b52c0ea3798
Luke-Clements <36176436+Luke-Clements@users.noreply.github.com>
2018-05-04 03:07:24 -0700

    Update SettingsInfo.java

--- Integration/SettingsInfo.java
+++ Integration/SettingsInfo.java
@@ -19,7 +19,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.SingleSelectionModel;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
